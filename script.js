5f374e13014450f67d5192d33d4374167963b38a
Grant Mulvena <thewiredoldsoul@protonmail.com>
2017-12-02 11:42:32 -0700

    Working JS
    ongoing

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-function idCard(){
+
+window.onload = function() {
+    var idCard;
+
+    function idCard(){
 	var fullname = document.getElementById("firstname" + "lastname").value;
     document.getElementById('postFullName').innerHTML = userInput;
     
-    // var firstname = document.getElementById("firstname").value;
-    // var lastname = document.getElementById("lastname").value;
     var address = document.getElementById("address").value;
     document.getElementById('postAddress').innerHTML = userInput;
 
     var age = document.getElementById("age").value;
     var telephonenumber = document.getElementById("telephonenumber").value;
+    
     var numberArray = [];
+    
     numberArray.push ("age", "telephonenumber");
-    for (numberArray=0){
-        if (numberArray > 100);
-        return address;
-        } else if (numberArray <100){
-        return age;
+    
+    for(numberArray = 0; i < 10; i++) {
+        if(i < 100) {
+            return(age);
         }
-    ]
-}
+        else(i > 100);
+            return(address);
+        }
+        }
+    }
+
+    // for (numberArray;{
+    //     if (document.getElementById("numberArray").value > 100);
+    //     return(address);
+    //     } else if (numberArray <100){
+    //     return age;
+    //     }
+    
+
+
