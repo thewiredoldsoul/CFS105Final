104e4d3962a7effc1bb50c3303f1bf9d2874386c
Grant Mulvena <thewiredoldsoul@protonmail.com>
2017-12-02 10:18:49 -0700

    Starting script file

---
+++ script.js
@@ -0,0 +1,21 @@
+function idCard(){
+	var fullname = document.getElementById("firstname" + "lastname").value;
+    document.getElementById('postFullName').innerHTML = userInput;
+    
+    // var firstname = document.getElementById("firstname").value;
+    // var lastname = document.getElementById("lastname").value;
+    var address = document.getElementById("address").value;
+    document.getElementById('postAddress').innerHTML = userInput;
+
+    var age = document.getElementById("age").value;
+    var telephonenumber = document.getElementById("telephonenumber").value;
+    var numberArray = [];
+    numberArray.push ("age", "telephonenumber");
+    for (numberArray=0){
+        if (numberArray > 100);
+        return address;
+        } else if (numberArray <100){
+        return age;
+        }
+    ]
+}
