8caf8bb9bad7632212a6788d0a75a66ef2375a7c
Grant Mulvena <thewiredoldsoul@protonmail.com>
2017-12-08 18:41:25 -0700

    Complete?
    Looking good.

--- script.js
+++ script.js
@@ -1,45 +1,71 @@
 
-window.onload = function() {
-    var idCard;
 
-    function idCard(){
-	var fullname = document.getElementById("firstname" + "lastname").value;
-    document.getElementById('postFullName').innerHTML = userInput;
+    var idCard;
     
+    function idCard(){
+    
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+  
+    var fullName = firstname + lastname;
+    document.getElementById("postFullName").innerHTML = fullName;
+
     var address = document.getElementById("address").value;
-    document.getElementById('postAddress').innerHTML = userInput;
+    document.getElementById("postAddress").innerHTML = address;
 
     var age = document.getElementById("age").value;
     var telephonenumber = document.getElementById("telephonenumber").value;
-    
+
     var numberArray = [];
     
-    numberArray.push ("age", "telephonenumber");
+    numberArray.push(age, telephonenumber);
     
     for(i = 0; i < numberArray.length; i++) {
-        if(numberArray[i] <= "age:" +i) {
-            document.getElementById("age").innerHTML = userInput;
-            
+        if(numberArray[i] <= 100) 
+        {
+            document.getElementById("postAge").innerHTML = ("Age:" + age) ;
         }
+        else
+        {
+            document.getElementById("postPhoneNumber").innerHTML = ("Telephone Number:" + telephonenumber);
         }
-        else (numberArray[i] >= "telephonenumber: " + userInput),{
-            document.getElementById("telephonenumber").innerHTML = telephonenumber;
-
-        return;
-            console.log(age);
-            console.log(address);
-
-
-        }
-    ]
+        console.log(age);
+        console.log(telephonenumber);
+        
+    }
     }
 
-    // for (numberArray;{
-    //     if (document.getElementById("numberArray").value > 100);
-    //     return(address);
-    //     } else if (numberArray <100){
-    //     return age;
-    //     }
+
+
+    // var fullname = document.getElementById("firstname" + "lastname").value;
+    // document.getElementById('postFullName').innerHTML = userInput;
+    
+    // var address = document.getElementById("address").value;
+    // document.getElementById('postAddress').innerHTML = userInput;
+
+    // var age = document.getElementById("age").value;
+    // var telephonenumber = document.getElementById("telephonenumber").value;
+    
+        
+        
+        
+    //     // else (numberArray[i] >= "telephonenumber: " + userInput),{
+    //     //     document.getElementById("telephonenumber").innerHTML = telephonenumber;
+
+    //     // return;
+    //     //     console.log(age);
+    //     //     console.log(address);
+
+
+    //     // }
     
 
-}
+    // // for (numberArray;{
+    // //     if (document.getElementById("numberArray").value > 100);
+    // //     return(address);
+    // //     } else if (numberArray <100){
+    // //     return age;
+    // //     }
+    
+
+    
